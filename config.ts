af3e91630ed850d7df3c384d355c148847e43b98
Evan Shortiss <evanshortiss@gmail.com>
2021-01-05 10:51:56 +0000

    chore: change max payload size to 1KB

--- config.ts
+++ config.ts
@@ -5,9 +5,9 @@ const { get } = require('env-var');
 const config = {
   HTTP_PORT: get('HTTP_PORT').default(3000).asPortNumber(),
 
-  // Reject web socket payloads greater than this many bytes (5KB by default)
+  // Reject web socket payloads greater than this many bytes (1KB by default)
   WS_MAX_PAYLOAD: get('WS_MAX_PAYLOAD')
-    .default(1024 * 5)
+    .default(1024)
     .asIntPositive(),
 };
 
