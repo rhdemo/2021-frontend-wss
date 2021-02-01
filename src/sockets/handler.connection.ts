760acd6ace5d8535138a6cc73a268e854107e730
Evan Shortiss <evanshortiss@gmail.com>
2021-02-01 12:15:51 +0000

    fix: notify players when both have been matched

--- src/sockets/handler.connection.ts
+++ src/sockets/handler.connection.ts
@@ -6,14 +6,14 @@ import { getMatchAssociatedWithPlayer } from '../matchmaking';
 import PlayerConfiguration, {
   PlayerConfigurationData
 } from '../models/player.configuration';
-import { initialisePlayer } from '../players';
+import * as players from '../players';
 import {
   ConnectionRequestPayload,
   MessageHandler,
   OutgoingMsgType,
   ValidationErrorPayload
 } from './payloads';
-import { getPlayerSpecificData } from './utils';
+import { getPlayerSpecificData, send } from './utils';
 
 const ConnectionRequestPayloadSchema = Joi.object({
   username: Joi.string(),
@@ -35,7 +35,10 @@ const connectionHandler: MessageHandler<
       }
     };
   } else {
-    const player = await initialisePlayer(ws, data as ConnectionRequestPayload);
+    const player = await players.initialisePlayer(
+      ws,
+      data as ConnectionRequestPayload
+    );
 
     const { opponent, match, game } = await getPlayerSpecificData(player);
 
@@ -43,6 +46,25 @@ const connectionHandler: MessageHandler<
       throw new Error(
         `failed to find match associated with player ${player.getUUID()}`
       );
+    }
+
+    if (opponent) {
+      // This player was added as the second player to a match, so we need
+      // to update the other player to let them know an opponent was found
+      const sock = players.getSocketForPlayer(opponent);
+      if (!sock) {
+        log.warn(
+          `unable to inform opponent (${opponent.getUUID()}) of player ${player.getUUID()} that the match is now ready, since their socket is missing. they must have disconnected`
+        );
+      } else {
+        log.info(
+          `sending configuration to opponent (${opponent.getUUID()}) of player ${player.getUUID()} since match is now ready`
+        );
+        send(sock, {
+          type: OutgoingMsgType.Configuration,
+          data: new PlayerConfiguration(game, opponent, match, player).toJSON()
+        });
+      }
     }
 
     return {
