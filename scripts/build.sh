01fedf165764c5afca16c2007d40be58a296f76a
Evan Shortiss <evanshortiss@gmail.com>
2021-02-03 11:16:23 +0000

    fix: build using local code

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 printf "\n\n######## game-server build ########\n"
 
-IMAGE_REPOSITORY=${PHONE_SERVER_IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-game-server:latest}
-SOURCE_REPOSITORY_URL=${SOURCE_REPOSITORY_URL:-git@github.com:rhdemo/2021-frontend-wss.git}
-SOURCE_REPOSITORY_REF=${SOURCE_REPOSITORY_REF:-master}
-
-echo "Building ${IMAGE_REPOSITORY} from ${SOURCE_REPOSITORY_URL} on ${SOURCE_REPOSITORY_REF}"
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-game-server:latest}
 
 s2i build -c . registry.access.redhat.com/ubi8/nodejs-14 ${IMAGE_REPOSITORY}
