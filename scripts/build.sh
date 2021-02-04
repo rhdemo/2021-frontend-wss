6711cd481b615da2e748094765f92d9e40d4ed4f
Evan Shortiss <evanshortiss@gmail.com>
2021-02-04 21:18:08 +0000

    fix: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,6 @@ printf "\n\n######## game-server build ########\n"
 
 IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-game-server:latest}
 
+rm -rf node_modules/
+
 s2i build -c . registry.access.redhat.com/ubi8/nodejs-14 ${IMAGE_REPOSITORY}
