fa309e925d783376c766b9d3af0a68ffcbfee7b7
Evan Shortiss <evanshortiss@gmail.com>
2021-02-11 11:36:39 +0000

    Update scripts/node/node.start.sh

--- scripts/node/node.start.sh
+++ scripts/node/node.start.sh
@@ -2,4 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -f Dockerfile.dev . -t summit-frontend-wss
 
-# docker run --rm --net=infinispan-docker-compose_summit -p 3000:3000 -v "$(pwd)/src/:/usr/src/app/src/" -e DATA --name=summit-frontend-wss  summit-frontend-wss
+docker run --rm --net=infinispan-docker-compose_summit -p 3000:3000 -v "$(pwd)/src/:/usr/src/app/src/" -e DATA --name=summit-frontend-wss  summit-frontend-wss
