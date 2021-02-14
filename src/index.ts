ca0b37f0b5fdd4380d644f2486d28b18198cf331
Evan Shortiss <evanshortiss@gmail.com>
2021-02-14 18:37:43 +0000

    chore: log prod/dev in startup

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import * as game from './game';
 import startServer from './server';
 import log from './log';
 import { configureHeartbeat } from './sockets';
+import { NODE_ENV } from './config';
 
 require('make-promises-safe');
 
 async function main() {
-  log.info('bootstrapping game server');
+  log.info(`bootstrapping game server in "${NODE_ENV}" mode`);
 
   await game.POST();
 
