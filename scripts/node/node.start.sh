8bce6af0df7dc089c12c312a522ddacc17e76a55
Evan Shortiss <evanshortiss@gmail.com>
2021-03-01 14:43:06 +0000

    fix: local dev script

--- scripts/node/node.start.sh
+++ scripts/node/node.start.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -f Dockerfile.dev . -t summit-frontend-wss
-
+npm install
 docker run --rm --net=infinispan-docker-compose_summit -p 3000:3000 -v "$(pwd)/src/:/usr/src/app/src/" -e DATA --name=summit-frontend-wss  summit-frontend-wss
