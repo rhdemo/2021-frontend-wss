2af1d77bd2a0aef77df7301ba92e5efd08cd2462
Evan Shortiss <evanshortiss@gmail.com>
2021-03-02 13:02:41 +0000

    fix: increase max payload size

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,8 @@ const config = {
   // HTTP and WebSocket traffic both use this port
   HTTP_PORT: get('HTTP_PORT').default(3000).asPortNumber(),
 
-  // Reject web socket payloads greater than this many bytes (1KB by default)
-  WS_MAX_PAYLOAD: get('WS_MAX_PAYLOAD').default(1024).asIntPositive(),
+  // Reject web socket payloads greater than this many bytes (2KB by default)
+  WS_MAX_PAYLOAD: get('WS_MAX_PAYLOAD').default(2048).asIntPositive(),
 
   // This is the grid size for the game, e.g "5" would produce a 5x5 grid
   GAME_GRID_SIZE: get('GAME_GRID_SIZE').default(5).asIntPositive(),
