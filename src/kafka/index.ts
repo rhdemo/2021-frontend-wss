91669aa94e857d45937682dee718d2af6b713e16
Evan Shortiss <evanshortiss@gmail.com>
2021-04-22 22:33:22 +0100

    fix: kafka producer undefined send issue

--- src/kafka/index.ts
+++ src/kafka/index.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-import { CloudEventBase, EventType } from "@app/cloud-events/types";
-import log from '@app/log'
+import { CloudEventBase, EventType } from '@app/cloud-events/types';
+import log from '@app/log';
 import { Kafka, KafkaConfig, Producer } from 'kafkajs';
 import {
   KAFKA_BOOTSTRAP_URL,
@@ -15,7 +15,9 @@ let kafka!: Kafka;
 let kafkaOpts!: KafkaConfig;
 let producer!: Producer;
 if (!KAFKA_BOOTSTRAP_URL || !KAFKA_SVC_USERNAME || !KAFKA_SVC_PASSWORD) {
-  log.warn('Kafka senders set to no-op. Set KAFKA_BOOTSTRAP_URL, KAFKA_SVC_USERNAME, KAFKA_SVC_PASSWORD to send data to Kafka')
+  log.warn(
+    'Kafka senders set to no-op. Set KAFKA_BOOTSTRAP_URL, KAFKA_SVC_USERNAME, KAFKA_SVC_PASSWORD to send data to Kafka'
+  );
 } else {
   kafkaOpts = {
     clientId: 'knative-match-event-forwarder',
@@ -29,18 +31,16 @@ if (!KAFKA_BOOTSTRAP_URL || !KAFKA_SVC_USERNAME || !KAFKA_SVC_PASSWORD) {
     ssl: {
       rejectUnauthorized: false
     }
-  }
-  kafka = new Kafka(kafkaOpts)
-  producer = kafka.producer()
+  };
+  kafka = new Kafka(kafkaOpts);
+  producer = kafka.producer();
 
   producer.on(producer.events.CONNECT, () => {
     log.info(`Kafka producer connected to broker ${KAFKA_BOOTSTRAP_URL}`);
   });
 
   producer.on(producer.events.DISCONNECT, (e) => {
-    log.error(
-      `Kafka producer disconnected from broker ${KAFKA_BOOTSTRAP_URL}`
-    );
+    log.error(`Kafka producer disconnected from broker ${KAFKA_BOOTSTRAP_URL}`);
     log.error(e);
     process.exit(1);
   });
@@ -50,7 +50,7 @@ if (!KAFKA_BOOTSTRAP_URL || !KAFKA_SVC_USERNAME || !KAFKA_SVC_PASSWORD) {
     log.error(e);
   });
 
-  producer.connect()
+  producer.connect();
 }
 
 const eventTypeMap: { [k in EventType]: string } = {
@@ -58,10 +58,10 @@ const eventTypeMap: { [k in EventType]: string } = {
   [EventType.Attack]: 'attack',
   [EventType.Bonus]: 'bonus',
   [EventType.MatchEnd]: 'end'
-}
+};
 
-export function send (data: CloudEventBase, type: EventType) {
-  if (kafka || type !== EventType.Bonus) {
+export function send(data: CloudEventBase, type: EventType) {
+  if (kafka && type !== EventType.Bonus) {
     const message = {
       key: `${data.game}:${data.match}`,
       value: JSON.stringify({ type: eventTypeMap[type], data, cluster })
@@ -72,12 +72,14 @@ export function send (data: CloudEventBase, type: EventType) {
     );
     log.trace(`sending payload to kafka: %j`, message);
 
-    return producer.send({
-      topic: KAFKA_TOPIC_MATCHES,
-      messages: [message]
-    }).catch((e) => {
-      log.error('error sending to kafka')
-      log.error(e)
-    });
+    return producer
+      .send({
+        topic: KAFKA_TOPIC_MATCHES,
+        messages: [message]
+      })
+      .catch((e) => {
+        log.error('error sending to kafka');
+        log.error(e);
+      });
   }
 }
